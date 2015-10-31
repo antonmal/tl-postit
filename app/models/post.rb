44fdd2e09e0ffbf52dd659af7a52da33787e5569
Anton Malkov <anton.malkov@gmail.com>
2015-10-31 09:05:52 +0100

    Redo the DB schema from scratch with hmt instead of habtm

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
-  belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
+  belongs_to :user
   has_many :comments
-  has_and_belongs_to_many :categories
+  has_many :post_categories
+  has_many :categories, through: :post_categories
 end
