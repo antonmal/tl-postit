389043338d6d8335ea56840b7781b7ae3d2e9d51
Anton Malkov <anton.malkov@gmail.com>
2015-11-02 16:32:33 +0100

    Add back all the files that should be tracked by git

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,21 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all order: 'updated_at DESC'
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+PostitTemplate::Application.routes.draw do
+  root to: 'posts#index'
+
+  # get 'posts', to: 'posts#index'
+  # get 'posts/:id', to: 'posts#show', as: 'post'
+  # get 'posts/new', to: 'posts#new'
+  # post 'posts', to: 'posts#create'
+  # get 'posts/:id/edit', to: 'posts#edit'
+  # patch 'posts/:id', to: 'posts#update'
+  # put 'posts/:id', to: 'posts#update'
+  # destroy 'posts/:id', to: 'posts#destroy'
+
+  resources :posts, except: [:destroy]
+end
