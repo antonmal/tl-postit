e1f32303005b8e74ae3571e282096abe4d4e482c
Anton Malkov <anton.malkov@gmail.com>
2015-11-18 11:38:26 +0100

    Connect to Heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PostitTemplate::Application.config.secret_key_base = 'c332402af7e4cc95b17800a17cd8080c76ac73c97a0b63ddc9984a8d8d10406a9e9992aec94a42513da501a11a6c8c335c8cc952f1d18f8b1ad4aeab699cea33'
